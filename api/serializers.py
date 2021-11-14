d79506ac3ae2a62b824daad982e2aef00532aec6
C8338Soner <sonerpeker84@gmail.com>
2021-11-15 01:16:07 +0300

    User create token

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Article
+from django.contrib.auth.models import User
+from rest_framework.authtoken.views import Token
 
 # class ArticleSerializer(serializers.Serializer):
 #   title = serializers.CharField(max_length=50)
@@ -16,3 +18,22 @@ class ArticleSerializer(serializers.ModelSerializer):
  class Meta:
   model = Article
   fields = '__all__'
+  
+class UserSerializer(serializers.ModelSerializer):
+ class Meta:
+  model = User
+  fields = ['id', 'username', 'password']
+  
+  # password görünmesin diye
+  extra_kwargs = {
+   'password':{
+       'write_only':True,
+       'required' : True
+   }
+  }
+
+ def create(self,validated_data):
+  user = User.objects.create_user(**validated_data)
+  Token.objects.create(user=user)
+  return user
+  
